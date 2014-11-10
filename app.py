3a4d616280d396b424e6d3a30d3be706791bbcd0
Eric Wong <wonggeric@gmail.com>
2014-11-10 11:47:41 -0500

    fixed app.py

--- app.py
+++ app.py
@@ -5,25 +5,21 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Please enter a tag into url with format '/t/what_city_you_want_the_weather_for'"
-
+    return render_template("home.html")
+        
 @app.route("/t/<tag>")
-def tag(tag="NewYork"):
-    #tag will be a city
-    #maybe use dictionary of city:state
-    #use tag to get city then put it into the url
-    city = tag
+def tag(city):
     state = list['city']
-    url="http://api.wunderground.com/api/e67a9f7ffa697198/geolookup/conditions/q/%s/%c.json"
+    url="http://api.wunderground.com/api/e67a9f7ffa697198/alerts/q/%s/%c.json"
     url = url%(city, state)
     request = urllib2.urlopen(url)
     resultstring = request.read()
     result = json.loads(resultstring)
-    location = result['location']['city']
-    temp_f = result['current_observation']['temp_f']
-    print "Curent temperature in %s is: %s" % (location, temp_f)
+    #getting the alert type
+    atype = result['alerts']['type']
     request.close()
 
 if __name__=="__main__":
     app.debug=True
-    app.run(host="0.0.0.0",port=8000)
+    app.run()
+    #app.run(host="0.0.0.0",port=8000)
